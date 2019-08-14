abe78be701f2c8220d7b992de60cd3c07b2bf819
Erin <erinedawson@gmail.com>
2019-08-14 16:56:01 -0600

    fix P and M constructors for real this time

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ class School{
   }
 }
 class Primary extends School{
-  constructor(name, level, numberOfStudents, pickupPolicy){
+  constructor(name, numberOfStudents, pickupPolicy){
     super(name, 'primary', numberOfStudents);
     this._pickupPolicy = pickupPolicy;
   }
@@ -41,7 +41,7 @@ class Primary extends School{
   }
 }
 class Middle extends School{
-  constructor(name, level, numberOfStudents){
+  constructor(name, numberOfStudents){
     super(name, 'middle', numberOfStudents);
   }
 }
