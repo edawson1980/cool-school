23c0622584d31d5754542ea2526734e00aa684cd
Erin <erinedawson@gmail.com>
2019-08-14 17:16:44 -0600

    successfully run testing

--- main.js
+++ main.js
@@ -58,3 +58,8 @@ class High extends School{
 //testing:
 const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 lorraineHansbury.quickFacts();
+
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J.R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball','Basketball','Volleyball','Track and Field']);
+console.log(alSmith.sportsTeams)
